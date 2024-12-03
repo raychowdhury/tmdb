5b711934004a77d7dd5f6fc8a7b94571646a7126
Ray <alvi.rayhan@gmail.com>
2024-12-03 18:32:15 +0400

    added Back to top button

--- src/app/movie/components/movie-default/movie-default.component.ts
+++ src/app/movie/components/movie-default/movie-default.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-movie-default',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./movie-default.component.scss']
 })
 export class MovieDefaultComponent {
+  showScrollTopButton = false;
 
+  constructor() {}
+
+  // Listen for window scroll events
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // If the user scrolls down 200px from the top, show the button
+    this.showScrollTopButton = window.scrollY > 200;
+  }
+
+  // Function to scroll the user to the top
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
